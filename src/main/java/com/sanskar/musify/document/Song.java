a1b04981093ce5abafaa27d6e45b4bdc232eff0f
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-04 17:03:17 +0530

    Feature: Created the song dto and song entity

---
+++ src/main/java/com/sanskar/musify/document/Song.java
@@ -0,0 +1,27 @@
+package com.sanskar.musify.document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "song")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Song {
+
+    @Id
+    @JsonProperty("_id")
+    private String id;
+    private String name;
+    private String description;
+    private String album;
+    private String image;
+    private String file;
+    private String duration;
+}
